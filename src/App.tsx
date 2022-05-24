b320e3872cbb4bb4aadc0340117c87a022af1e14
dmitry <bdodinka1992@gmail.com>
2022-05-24 19:07:58 +0300

    feat:add four Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import './App.css';
+import {TodoList} from "./components/Todolist";
 
-function App() {
+
+export function App() {
+
+    const tasks_1 = [
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'REACT', isDone: true}
+    ]
+    const tasks_2 = [
+        {id: 1, title: 'Meet', isDone: false},
+        {id: 2, title: 'Chicken', isDone: true},
+        {id: 3, title: 'Whisky', isDone: true}
+    ]
+    const tasks_3 = [
+        {id: 1, title: 'MORBIUS', isDone: true},
+        {id: 2, title: 'SPIDER MAN', isDone: true},
+        {id: 3, title: 'HALO', isDone: false}
+    ]
+    const tasks_4 = [
+        {id: 1, title: 'Rumba', isDone: false},
+        {id: 2, title: 'Salsa', isDone: false},
+        {id: 3, title: 'Tango', isDone: false}
+    ]
+
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" defaultChecked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" defaultChecked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" defaultChecked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title="What to do?" tasks={tasks_1}/>
+            <TodoList title="What to Buy?" tasks={tasks_2}/>
+            <TodoList title="What to Watch?" tasks={tasks_3}/>
+            <TodoList title="What to dance?" tasks={tasks_4}/>
         </div>
-    );
-}
 
-export default App;
+    )
+}
